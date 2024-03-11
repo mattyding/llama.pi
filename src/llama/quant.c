b278bf7f44b1d9d8f4aa4e3b02793e1060d43c5f
mattyding <66889540+mattyding@users.noreply.github.com>
2024-03-11 01:31:22 -0700

    split runq.c into separate files

---
+++ src/llama/quant.c
@@ -0,0 +1,60 @@
+// quantization helper functions
+#include<math.h>
+#include<stdint.h>
+
+int GS = 0; // group size global for quantization of the weights
+
+typedef struct {
+    int8_t* q;    // quantized values
+    float* s; // scaling factors
+} QuantizedTensor;
+
+void dequantize(QuantizedTensor *qx, float* x, int n) {
+    for (int i = 0; i < n; i++) {
+        x[i] = qx->q[i] * qx->s[i / GS];
+    }
+}
+
+void quantize(QuantizedTensor *qx, float* x, int n) {
+    int num_groups = n / GS;
+    float Q_MAX = 127.0f;
+
+    for (int group = 0; group < num_groups; group++) {
+
+        // find the max absolute value in the current group
+        float wmax = 0.0;
+        for (int i = 0; i < GS; i++) {
+            float val = fabs(x[group * GS + i]);
+            if (val > wmax) {
+                wmax = val;
+            }
+        }
+
+        // calculate and write the scaling factor
+        float scale = wmax / Q_MAX;
+        qx->s[group] = scale;
+
+        // calculate and write the quantized values
+        for (int i = 0; i < GS; i++) {
+            float quant_value = x[group * GS + i] / scale; // scale
+            int8_t quantized = (int8_t) round(quant_value); // round and clamp
+            qx->q[group * GS + i] = quantized;
+        }
+    }
+}
+
+/* initialize `n` x quantized tensor (with `size_each` elements), starting from memory pointed at *ptr */
+// QuantizedTensor *init_quantized_tensors(void **ptr, int n, int size_each) {
+//     void *p = *ptr;
+//     QuantizedTensor *res = kmalloc(n * sizeof(QuantizedTensor));
+//     for(int i=0; i<n; i++) {
+//         /* map quantized int8 values*/
+//         res[i].q = (int8_t*)p;
+//         p = (int8_t*)p + size_each;
+//         /* map scale factors */
+//         res[i].s = (float*)p;
+//         p = (float*)p + size_each / GS;
+//     }
+//     *ptr = p; // advance ptr to current position
+//     return res;
+// }
